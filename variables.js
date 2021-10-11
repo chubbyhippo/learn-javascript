b9b722e808370898ba663a512647ccce02d6b73a
ch <mkmsacc@outlook.com>
2021-10-11 22:00:04 +0700

    Add variables.js

---
+++ variables.js
@@ -0,0 +1,14 @@
+var firstName = "Douglas";
+var age = 85;
+var isFemale = false;
+var balance = 100.32;
+var dob = new Date(2021, 12, 9);
+var person = {};
+var empty = undefined;
+
+console.log(firstName);
+console.log(age);
+console.log(isFemale);
+console.log(balance);
+console.log(dob);
+console.log(empty);
