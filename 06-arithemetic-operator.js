734862c833d235d238520054c1fbd8872355a7e3
ch <mkmsacc@outlook.com>
2021-10-12 17:10:53 +0700

    Add 06

---
+++ 06-arithemetic-operator.js
@@ -0,0 +1,13 @@
+var addition = 1 + 1;
+var subtraction = 1 - 1;
+var division = 23 / 6;
+var multiplication = 23 * 6;
+var remainder = 10 % 7;
+var exponentation = 2 ** 64;
+
+console.log(addition);
+console.log(subtraction);
+console.log(division);
+console.log(multiplication);
+console.log(remainder);
+console.log(exponentation);
