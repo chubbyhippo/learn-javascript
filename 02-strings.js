795926f12c4abef0b0a817ab7205fb908ff851d4
ch <mkmsacc@outlook.com>
2021-10-12 07:55:03 +0700

    Add strings

---
+++ 02-strings.js
@@ -0,0 +1,10 @@
+var animal = "hippo";
+console.log(typeof animal);
+console.log(animal.length);
+console.log(animal.toUpperCase());
+console.log(animal.substring(0, 3));
+
+var a = "hip";
+var b = "po";
+console.log(a + b);
+console.log(`${a}${b.toUpperCase()}`);
