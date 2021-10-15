8fddee940e9215a392d4c476e8f49e8e7043b0bc
ch <mkmsacc@outlook.com>
2021-10-15 15:58:41 +0700

    Add ternary

---
+++ 10-ternary.js
@@ -0,0 +1,10 @@
+const number = 42;
+if (0 === number % 2) {
+    console.log('even');
+} else {
+    console.log('odd');
+}
+
+const result = number % 2 === 0 ? 'even' : 'odd';
+
+console.log(result);
